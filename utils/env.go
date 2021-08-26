ea6105b61c9a1dd68f1b4ee2a89eac84b3b1cf90
liuzehao <123456@123.com>
2021-08-26 17:53:41 +0800

    add the test path and actual path variables

--- utils/env.go
+++ utils/env.go
@@ -3,14 +3,30 @@ package utils
 import (
 	"errors"
 	"github.com/joho/godotenv"
+	"log"
 	"os"
+	"os/exec"
 	"postbar/datamodels"
 	err2 "postbar/err"
+	"strings"
+)
+
+func getCurrentPath() string {
+	s, _ := exec.LookPath(os.Args[0])
+	i := strings.LastIndex(s, "/")
+	path := string(s[0 : i+1])
+	return path
+}
+
+var (
+	tmpEnvPah  = getCurrentPath() + "envs/dbs.env"
+	ActualPath = "../envs/dbs.env"
 )
 
 func init() {
-	err := godotenv.Load("../envs/dbs.env")
-	err2.Reciteerr(&err)
+	log.Println("Path", getCurrentPath())
+	err := godotenv.Load(tmpEnvPah)
+	err2.ReciteErr(&err)
 }
 
 func GetMySqlENV() *datamodels.MySQLENV {
@@ -27,8 +43,8 @@ func GetMongoENV() (string, string, error) {
 	host := os.Getenv("MONGO_HOST")
 	port := os.Getenv("MONGO_PORT")
 	if len(host) == 0 || len(port) == 0 {
-		errs := errors.New("error while getting environment")
-		err2.Reciteerr(&errs)
+		errs := errors.New("error while getting environment variables")
+		err2.ReciteErr(&errs)
 		return "", "", errs
 	}
 	return host, port, nil
