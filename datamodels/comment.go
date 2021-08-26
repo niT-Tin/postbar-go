6b325f903f8f5f81f3dec1320668335366c2342c
liuzehao <123456@123.com>
2021-08-26 08:15:52 +0800

    comment models

---
+++ datamodels/comment.go
@@ -0,0 +1,14 @@
+package datamodels
+
+import "time"
+
+type Comment struct {
+	ID          int64     `bson:"id"`
+	CommentId   int64     `bson:"comment_id"`
+	PublisherId int64     `bson:"publisher_id"`
+	SubId       int64     `bson:"sub_id"`
+	ParentId    int64     `bson:"parent_id"`
+	PublishDate time.Time `bson:"publish_date"`
+	ReplyNUm    int64     `bson:"reply_num"`
+	Contents    Content   `bson:"contents"`
+}
