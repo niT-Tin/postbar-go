2998fce9644483b17637965f36a17bacac4c743e
liuzehao <123456@123.com>
2021-08-26 14:12:10 +0800

    rename err func

--- err/reciteerr.go
+++ err/reciteerr.go
@@ -2,7 +2,7 @@ package err
 
 import "log"
 
-func Reciteerr(err *error) bool {
+func ReciteErr(err *error) bool {
 	if *err != nil {
 		log.Printf("error opening database: %v", err)
 		// TODO: 将错误信息写入RabbitMQ队列，进而将错误消息一个一个插入mysql数据库
