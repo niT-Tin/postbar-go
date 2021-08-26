c5bebbc73a2307c298be05c1480a61306c1ffb9a
liuzehao <123456@123.com>
2021-08-26 17:55:31 +0800

    fix postAvatarUrl type

--- datamodels/postbar.go
+++ datamodels/postbar.go
@@ -1,10 +1,10 @@
 package datamodels
 
 type PostBar struct {
-	ID         int64 `bson:"id"`
-	PostBarId  int64 `bson:"postbar_id"`
-	PCount     int64 `bson:"pcount"`
-	MainId     int64 `bson:"main_id"`
-	NewInc     int64 `bson:"new_inc"`
-	PostAvatar int64 `bson:"post_avatar"`
+	ID         int64  `bson:"id"`
+	PostBarId  int64  `bson:"postbar_id"`
+	PCount     int64  `bson:"pcount"`
+	MainId     int64  `bson:"main_id"`
+	NewInc     int64  `bson:"new_inc"`
+	PostAvatar string `bson:"post_avatar"`
 }
