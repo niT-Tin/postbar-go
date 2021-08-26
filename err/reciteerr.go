86e56c0b250299867d4197b26630295fa3f57635
liuzehao <123456@123.com>
2021-08-26 17:56:14 +0800

    fix print err msg

--- err/reciteerr.go
+++ err/reciteerr.go
@@ -4,7 +4,7 @@ import "log"
 
 func ReciteErr(err *error) bool {
 	if *err != nil {
-		log.Printf("error opening database: %v", err)
+		log.Printf("error opening database: %v", *err)
 		// TODO: 将错误信息写入RabbitMQ队列，进而将错误消息一个一个插入mysql数据库
 		return true
 	}
